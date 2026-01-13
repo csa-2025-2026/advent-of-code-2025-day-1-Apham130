b2dde000608ce138a5a343e5fbfc701ec516f823
Apham130 <andypham4747@gmail.com>
2026-01-13 21:36:18 +0000

    day 3 pt 2

--- Day3/Day3.java
+++ Day3/Day3.java
@@ -8,8 +8,23 @@ public class Day3
   {
     File file = new File("day3.txt");  
     Scanner sc = new Scanner(file);   
-    String line = sc.nextLine();
-    long answer = 0;
+    int answer = 0;
+    while (sc.hasNext()) {
+      String line = sc.nextLine();
+      String m1 = "0";
+      int imax = 0;
+      for (int h = 11; h >= 0; h--) {
+        for (int i = imax; i < line.length() - h; i++) {
+          int num = Integer.parseInt(line.substring(i, i + 1));
+          String snum = line.substring(i, i + 1);
+          if(num > Integer.parseInt(m1)) {
+            imax = i;
+            m1 += snum;
+          }
+        }
+      answer += Integer.parseInt(m1); 
+      }
+    }
     System.out.println("The answer is " + answer);
     sc.close();
   }
